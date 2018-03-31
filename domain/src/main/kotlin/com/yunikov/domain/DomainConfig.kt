a860180ed195770f1c0679d2a586909082305806
yyunikov <yyunikov@gmail.com>
2018-03-31 20:13:01 +0300

    Fixed component scan

--- domain/src/main/kotlin/com/yunikov/domain/DomainConfig.kt
+++ domain/src/main/kotlin/com/yunikov/domain/DomainConfig.kt
@@ -1,6 +1,8 @@
 package com.yunikov.domain
 
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
+@ComponentScan(basePackages = ["com.yunikov.domain"])
 @Import(MongoConfig::class)
 class DomainConfig
