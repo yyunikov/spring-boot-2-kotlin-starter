fe83b477d3f514c758e04d40d7b65100461028b1
yyunikov <yyunikov@gmail.com>
2018-04-01 14:11:18 +0300

    Improved router function

--- api/src/main/kotlin/com/yunikov/api/WebConfig.kt
+++ api/src/main/kotlin/com/yunikov/api/WebConfig.kt
@@ -7,21 +7,25 @@ import org.springframework.http.MediaType
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.server.HandlerFunction
 import org.springframework.web.reactive.function.server.RequestPredicates
-import org.springframework.web.reactive.function.server.RequestPredicates.GET
-import org.springframework.web.reactive.function.server.RequestPredicates.accept
 import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.RouterFunctions.nest
 import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.router
 
 open class WebConfig: WebFluxConfigurer {
 
     @Bean
     fun apiRouter(testHandler: TestHandler,
-                  errorHandler: ErrorHandler): RouterFunction<*> {
-        return nest(accept(MediaType.APPLICATION_JSON_UTF8),
-                route(GET(TEST_PATH), HandlerFunction { request -> testHandler.test(request) })
-        ).andOther(route(RequestPredicates.all(), HandlerFunction { errorHandler.notFound() }))
-    }
+                  errorHandler: ErrorHandler): RouterFunction<*> = router {
+        (accept(MediaType.APPLICATION_JSON_UTF8).nest {
+            GET(TEST_PATH, testHandler::test )
+
+            // nest paths like:
+            //"/test2".nest {
+            // GET(TEST2_PATH, { request -> testHandler.test(request)
+            // POST(TEST2_PATH, { request -> testHandler.test(request)
+            // }) }
+        })
+    }.andOther(route(RequestPredicates.all(), HandlerFunction { errorHandler.notFound() }))
 
     companion object {
         const val TEST_PATH = "/test"
